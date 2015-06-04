185ec40eda5184c9631aa04839a1805575c6515d
bananabatman <ellas@kth.se>
2015-06-04 19:31:59 +0200

    small fixes

--- php/getcomp.php
+++ php/getcomp.php
@@ -3,15 +3,8 @@
 	include_once("functions.php");
 
 	
-	$q = "SELECT info FROM company WHERE cname = '".$_GET['cname']."';";
+	$q = "SELECT info FROM company WHERE cname = '".utf8_decode($_GET['cname'])."';";
 	$result = queryDb($conn, $q);
-	//$companies = array();
-
-	// while($row = $result->fetch_object()) {
-	// 	$company=utf8_encode($row->info);
-	// 	array_push($companies, $company);
-	// }
-
 	$info = utf8_encode($result->fetch_object()->info);
 
     echo json_encode($info);
