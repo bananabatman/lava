5de6740257e44ead8feece75d9eeab9c12c02be0
bananabatman <ellas@kth.se>
2015-06-04 20:48:50 +0200

    changed fav queries

--- php/fav.php
+++ php/fav.php
@@ -2,18 +2,19 @@
 	include_once("dbconf.php");
 	include_once("functions.php");
 
-	$input = $_POST;
-	$q = "SELECT count(*) AS bookmark, cid FROM company WHERE cname='".$_POST['cname']."';";
+	$q = "SELECT cid FROM company WHERE cname='".$_POST['cname']."';";
+	$cid = queryDb($conn, $q)->fetch_object()->cid;
+	
+	$q = "SELECT count(*) AS bookmark FROM company WHERE cid=".$cid." AND uid=".$_POST['uid']";";
 	$result=queryDb($conn, $q)->fetch_object();
 	$bookmark = $result->bookmark;
-	$results = $result->cid;
 	
 	if($bookmark==0) {
-		$q = "INSERT INTO bookmarks (uid, cid, cname) VALUES(".$_POST['uid'].", ".$results.", '".$_POST['cname']."');";
+		$q = "INSERT INTO bookmarks (uid, cid, cname) VALUES(".$_POST['uid'].", ".$cid.", '".$_POST['cname']."');";
 		queryDb($conn, $q);
 		echo json_encode(true);
 	} else  {
-		$q = "DELETE FROM bookmarks WHERE uid=".$_POST['uid']." AND cid=".$results.";";
+		$q = "DELETE FROM bookmarks WHERE uid=".$_POST['uid']." AND cid=".$cid.";";
 		queryDb($conn, $q);
 		echo json_encode(false);
 	}
